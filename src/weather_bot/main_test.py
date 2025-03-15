0eb2296056c27348d85ef7751f69dc3db4d2962b
Clément Sicard <clement.sicard@icloud.com>
2025-03-15 22:01:54 +0100

    fix tests 2

--- src/weather_bot/main_test.py
+++ src/weather_bot/main_test.py
@@ -7,7 +7,9 @@ from weather_bot.main import main
 
 @mock.patch("weather_bot.main.MeteoSwiss.is_raining_today", side_effect=True)
 @mock.patch("weather_bot.main.TelegramBot.send_telegram_message")
+@mock.patch("weather_bot.main.Env")
 def test_e2e(
+    mocked_env_loading: mock.Mock,
     mocked_telegram_bot: mock.Mock,
     mocked_meteoswiss_call: mock.Mock,
 ) -> None:
